41fbfb373439ea993c962e2450ba64eed63ef461
Gandls <omgjustmakeanameforme@gmail.com>
2021-03-12 14:17:06 -0500

    Added Switches between states and placeholder text, Also added helper methods for single key presses and single mouse clicks

--- MainGame/MainGame/Game1.cs
+++ MainGame/MainGame/Game1.cs
@@ -29,6 +29,8 @@ namespace MainGame
         private SpriteFont Subtitle;
         private SpriteFont Normal;
         private List<Color> Colors;
+        private KeyboardState KBS;
+        private KeyboardState prevKBS;
         private int c;
         private int shift;
 
@@ -47,6 +49,7 @@ namespace MainGame
             c = 0;
             shift = 0;
             Colors = new List<Color>(6) { Color.Red, Color.DarkOrange, Color.Yellow, Color.Green, Color.Blue, Color.Violet };
+            this.prevKBS = Keyboard.GetState();
 
             base.Initialize();
         }
@@ -71,6 +74,7 @@ namespace MainGame
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            KBS = Keyboard.GetState();
 
             // TODO: Add your update logic here
 
@@ -83,25 +87,53 @@ namespace MainGame
                         shift++;
                     }
                     c++;
-
+                    //This handles the switches between states
+                    if (SingleKeyPress(Keys.N, KBS, prevKBS))
+                    {
+                        currentState = GameState.Normal;
+                    }
+                    else if (SingleKeyPress(Keys.H, KBS, prevKBS))
+                    {
+                        currentState = GameState.Hard;
+                    }
+                    else if (SingleKeyPress(Keys.S, KBS, prevKBS))
+                    {
+                        currentState = GameState.Speedrun;
+                    }
                     break;
 
                 case GameState.Normal:
+                    if (SingleKeyPress(Keys.Enter, KBS, prevKBS))
+                    {
+                        currentState = GameState.Title;
+                    }
                     break;
 
                 case GameState.Hard:
+                    if (SingleKeyPress(Keys.Enter, KBS, prevKBS))
+                    {
+                        currentState = GameState.Title;
+                    }
                     break;
 
                 case GameState.Speedrun:
+                    if (SingleKeyPress(Keys.Enter, KBS, prevKBS))
+                    {
+                        currentState = GameState.Title;
+                    }
                     break;
 
                 case GameState.GameOver:
+                    if (SingleKeyPress(Keys.Enter, KBS, prevKBS))
+                    {
+                        currentState = GameState.Title;
+                    }
                     break;
 
                 default:
                     break;
             }
-
+            prevKBS = Keyboard.GetState();
             base.Update(gameTime);
         }
 
@@ -118,30 +150,37 @@ namespace MainGame
                 case GameState.Title:
                     GraphicsDevice.Clear(Color.Black);
 
-                    _spriteBatch.Begin();
                     _spriteBatch.DrawString(frontLayer, "Giraffe Noise 2", new Vector2(35, 7), Color.OrangeRed);
                     _spriteBatch.DrawString(Subtitle, "Choose Mode: ", new Vector2(35, 187), Color.Green);
-                    _spriteBatch.DrawString(Normal, "Normal\nHard", new Vector2(35, 237), Color.Gray);
+                    _spriteBatch.DrawString(Normal, "Normal - Press 'N'\nHard - Press 'H'", new Vector2(35, 237), Color.Gray);
                     _spriteBatch.DrawString(frontLayer, "B", new Vector2(35, 87 + (15 * (float)Math.Sin(c / 6))), Colors.ToArray()[(shift) % Colors.Count]);
                     _spriteBatch.DrawString(frontLayer, "E", new Vector2(95, 87 + (15 * (float)Math.Sin(c / 6 + 30))), Colors.ToArray()[(1 + shift) % Colors.Count]);
                     _spriteBatch.DrawString(frontLayer, "G", new Vector2(155, 87 + (15 * (float)Math.Sin(c / 6 + 60))), Colors.ToArray()[(2 + shift) % Colors.Count]);
                     _spriteBatch.DrawString(frontLayer, "I", new Vector2(235, 87 + (15 * (float)Math.Sin(c / 6 + 90))), Colors.ToArray()[(3 + shift) % Colors.Count]);
                     _spriteBatch.DrawString(frontLayer, "N", new Vector2(275, 87 + (15 * (float)Math.Sin(c / 6 + 120))), Colors.ToArray()[(4 + shift) % Colors.Count]);
                     _spriteBatch.DrawString(frontLayer, "?", new Vector2(345, 87 + (15 * (float)Math.Sin(c / 6 + 150))), Colors.ToArray()[(5 + shift) % Colors.Count]);
-                    _spriteBatch.DrawString(Normal, "Speedrun", new Vector2(35, 307), Color.Gray);
+                    _spriteBatch.DrawString(Normal, "Speedrun - Press 'S'", new Vector2(35, 307), Color.Gray);
 
                     break;
 
                 case GameState.Normal:
+                    _spriteBatch.DrawString(frontLayer, "Placeholder for \nNormal mode", new Vector2(35, 7), Color.OrangeRed);
+                    _spriteBatch.DrawString(Normal, "To exit, press enter", new Vector2(35, 307), Color.Yellow);
                     break;
 
                 case GameState.Hard:
+                    _spriteBatch.DrawString(frontLayer, "Placeholder for \nHard mode", new Vector2(35, 7), Color.OrangeRed);
+                    _spriteBatch.DrawString(Normal, "To exit, press enter", new Vector2(35, 307), Color.Yellow);
                     break;
 
                 case GameState.Speedrun:
+                    _spriteBatch.DrawString(frontLayer, "Placeholder for \nSpeedrun mode", new Vector2(35, 7), Color.OrangeRed);
+                    _spriteBatch.DrawString(Normal, "To exit, press enter", new Vector2(35, 307), Color.Yellow);
                     break;
 
                 case GameState.GameOver:
+                    _spriteBatch.DrawString(frontLayer, "Placeholder for \nGameover mode", new Vector2(35, 7), Color.OrangeRed);
+                    _spriteBatch.DrawString(Normal, "To exit, press enter", new Vector2(35, 307), Color.Yellow);
                     break;
 
                 default:
@@ -151,6 +190,15 @@ namespace MainGame
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+        protected bool SingleKeyPress(Keys key, KeyboardState kbs, KeyboardState prevkbs)
+        {
+            return (kbs.IsKeyUp(key) && prevkbs.IsKeyDown(key));
+        }
+
+        protected bool SingleMousePress(ButtonState b, MouseState ms, MouseState prevms)
+        {
+            return (ms.LeftButton != b && prevms.LeftButton == b);
         }
     }
 }
