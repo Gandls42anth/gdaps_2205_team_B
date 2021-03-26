2268fe58333e742345f6530bf5648a1bbdf5ea2b
Gandls <omgjustmakeanameforme@gmail.com>
2021-03-26 14:14:39 -0400

    Fixed a few syntax errors in the level class

--- MainGame/MainGame/Level.cs
+++ MainGame/MainGame/Level.cs
@@ -27,46 +27,43 @@ namespace MainGame
             bool curAdd = false;
 
             //This is where the random generation logic is handled for levels
-            if (gs == GameState.Normal) {
+            if (gs == GameState.Normal)
+            {
                 int guards = randy.Next(5 + levelNum * 2);
                 int guardsAdded = 0;
                 int positionsNum = (5 + levelNum) * 3;
-                guard = new bool[5,positionsNum];
-            for (int i = 0; i < positionsNum; i++)
-
-
+                guard = new bool[5, positionsNum];
+                for (int i = 0; i < positionsNum; i++)
                 {
 
 
-                    for(int p = 0; p < 5; p++)
                     {
-                        //This is a bit complicated to explain, but it allows for reasonable distribution of the guards, balancing
-                        //The amount of guards left with the amount of spots left on the map
-                        //It also favors creating them on different rows instead of different columns
-                        //(Since all of them on different rows would create an easier game)
-                        if (guards != 0)
+                        for (int p = 0; p < 5; p++)
                         {
-                            curAdd = randy.Next((positionsNum * 5) - (i * 5) - p) < guards;
-                            guard[p, i] = curAdd;
-                            if (curAdd)
+                            //This is a bit complicated to explain, but it allows for reasonable distribution of the guards, balancing
+                            //The amount of guards left with the amount of spots left on the map
+                            //It also favors creating them on different rows instead of different columns
+                            //(Since all of them on different rows would create an easier game)
+                            if (guards != 0)
                             {
-                                guards -= 1;
-                                guardsAdded += 1;
+                                curAdd = randy.Next((positionsNum * 5) - (i * 5) - p) < guards;
+                                guard[p, i] = curAdd;
+                                if (curAdd)
+                                {
+                                    guards -= 1;
+                                    guardsAdded += 1;
+                                }
                             }
-                        }
-                        else
-                        //If we're all out of guard to give, the remaining spots will be set to false
-                        {
-                            guard[p, i] = false;
+                            else
+                            //If we're all out of guard to give, the remaining spots will be set to false
+                            {
+                                guard[p, i] = false;
+                            }
                         }
                     }
                 }
-
-
-
-
-            //This is the same code as before, with altered coefficients to create longer, denser maps
-            }else if(gs == GameState.Hard)
+            }
+            else if (gs == GameState.Hard)
             {
                 {
                     int guards = randy.Next(5 + levelNum * 3);
@@ -103,7 +100,7 @@ namespace MainGame
             else
             {
                 //This code is never supposed to run
-                guard = new bool[0,0];
+                guard = new bool[0, 0];
             }
         }
 
