5677856c33fdd80deab3f9e41e2fa20355b60549
Sean Alexis Balayon <seanbalayon@gmail.com>
2021-04-23 14:24:40 -0400

    friendly npc class commit/push

---
+++ MainGame/MainGame/Filler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainGame
+{
+    class Filler
+    {
+        /*
+         * THIS IS A FILLER CLASS SO I CAN PUT VISUAL STUDIO ON MY MONITOR
+         * 
+         * NO STEALING OUR CODE
+         * 
+         * STOP READING THIS GO AWAY
+         * 
+         * WHAT YOU'RE READING NOW DOESN'T ACTUALLY EXIST
+         * 
+         * THIS TEXT IS AN ILLUSION
+         */
+    }
+}
--- MainGame/MainGame/NPC.cs
+++ MainGame/MainGame/NPC.cs
@@ -9,10 +9,32 @@ namespace MainGame
 {
     class NPC : GameObject
     {
+        // fields
+        private Rectangle npcRect;
+        private Texture2D npcTexture;
+        private string message;
 
-        public NPC(Rectangle rect, Texture2D txt, string Message) : base(rect,txt)
+        // delegate to handle interaction with the npc
+        public delegate void FriendlyNPCInteraction(string a);
+        private FriendlyNPCInteraction niceInteraction;
+
+        // properties
+        public Rectangle NPCRect { get { return npcRect; } }
+        public Texture2D NPCTexture { get { return npcTexture; } }
+        public string Message
         {
-
+            get { return message; }
+            set { message = value; }
         }
+
+        // constructor
+        public NPC(Rectangle rect, Texture2D txt, string message) : base(rect,txt)
+        {
+            this.npcRect = rect;
+            this.npcTexture = txt;
+            this.message = message;
+        }
+
+        
     }
 }
